4e6a073469ab23c7a29300c8e6ed21fa7d1ed15d
wonchul <onedang22@gmail.com>
2024-10-05 12:21:13 +0900

    change to load image files

--- src/yolo/val.py
+++ src/yolo/val.py
@@ -1,10 +1,13 @@
-from glob import glob 
+import numpy as np
+from glob import glob
+from PIL import Image, ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+import numpy as np
 import os.path as osp
 import os
 from ultralytics import YOLO
 import math
 import csv
-import cv2
 from pathlib import Path
 from tqdm import tqdm
 ROOT = Path(__file__).resolve()
@@ -21,16 +24,19 @@ def make_submission(input_dir='/HDD/datasets/dota/val/images/part1-001/images',
     model = YOLO(FILE / Path("yolox-obb.pt"))
     # model = YOLO("yolo11n-obb.pt")
     
-    # img_files = glob(osp.join(input_dir, '*.png'))
     img_files = glob(f'{input_dir}/**/*.png', recursive=True)
-    print(os.listdir(input_dir))
+    print("input_dir: ", input_dir)
     print(img_files)
-
+    
     results = []
-    for img_file in img_files:
-        img = cv2.imread(img_file)
+    for idx, img_file in enumerate(img_files):
+        image_name = osp.split(img_file)[-1]
+        print(f"{idx}: {image_name}")
+        with Image.open(img_file) as image:
+            img = np.array(image, dtype=np.uint8)
+
         img_h, img_w, img_c = img.shape
-        cnt = 0
+        print(f"   - h, w: {img_h}, {img_w}")
         for y0 in tqdm(range(0, img_h, dy)):
             for x0 in range(0, img_w, dx):
                 if y0 + patch_imgsz > img_h:
@@ -46,7 +52,7 @@ def make_submission(input_dir='/HDD/datasets/dota/val/images/part1-001/images',
 
                 xmin, xmax, ymin, ymax = x, x + patch_imgsz, y, y + patch_imgsz
                 patch = img[ymin:ymax, xmin:xmax, :]
-                output = model(patch, save=False, imgsz=patch_imgsz, conf=confidence_threshold, verbose=False)[0]
+                output = model(patch, save=True, imgsz=patch_imgsz, conf=confidence_threshold, verbose=False)[0]
                 obb_result = output.obb
                 classes = obb_result.cls.tolist()
                 xywhrs = obb_result.xywhr
@@ -58,9 +64,6 @@ def make_submission(input_dir='/HDD/datasets/dota/val/images/part1-001/images',
                             result.update({'cx': cx.item() + xmin, 'cy': cy.item() + ymin, 
                                         'width': width.item(), 'height': height.item(), 
                                         'angle': math.degrees(angle.item())})
-                            # cv2.imwrite(f"/HDD/etc/ship_result/patch_{cnt}.png", patch)
-                            # cv2.imwrite(f"/HDD/etc/ship_result/output_{cnt}.png", output.plot())
-                            # cnt += 1
                     results.append(result)
         
     with open(FILE / Path("submission.csv"), mode='w', newline='') as file:
